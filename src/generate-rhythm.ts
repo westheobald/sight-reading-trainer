e3bd5371ec02175bc521429d469bc867f58fac83
Wes Theobald <theobald.wes@gmail.com>
2023-11-12 17:30:09 -0800

    Changed data structure for holding rhythmKey to array of objects (instead of array of array/tuplets)

--- src/generate-rhythm.ts
+++ src/generate-rhythm.ts
@@ -1,7 +1,7 @@
 import { timeSignatureSplit, rhythmKey } from "./constants";
 import { getRandomIndex } from "./helpers";
 
-export function generateRhythms(numBars: number, timeSignature: [number, number]): [string, number][][] {
+export function generateRhythms(numBars: number, timeSignature: [number, number]): rhythmKey[][] {
   const rhythms = [];
   for (let _ = 0; _ < numBars; _++) {
     rhythms.push(generateOneBar(timeSignature));
@@ -9,34 +9,30 @@ export function generateRhythms(numBars: number, timeSignature: [number, number]
   return rhythms;
 }
 
-export function generateOneBar([numerator, denominator]: [number, number]): [string, number][] {
-  const minRhythmVal = rhythmKey.find(([str]) => {
-    if (str == 'q') str = '4';
-    if (str == 'h') str = '2';
-    if (+str == denominator) return true;
-  });
+export function generateOneBar([numerator, denominator]: [number, number]): rhythmKey[] {
+  const baseRhythmValue = rhythmKey.find(rhythmObj => denominator == rhythmObj.number)?.value;
   const split = timeSignatureSplit[numerator];
-  if (!split || !minRhythmVal) throw Error('Time signature not valid.');
-  let remaining = minRhythmVal[1] * numerator;
+  if (!split || !baseRhythmValue) throw Error('Time signature not valid.');
+  let remaining = baseRhythmValue * numerator;
 
-  const bar: [string, number][] = [];
+  const bar: rhythmKey[] = [];
 
   // TODO: Randomize split order? Reverse?
   if (split.length > 1) {
     for (const splitNumerator of split) {
       bar.push(...generateOneBar([splitNumerator, denominator]));
-      remaining -= minRhythmVal[1] * splitNumerator;
+      remaining -= baseRhythmValue * splitNumerator;
     }
   } else {
     const possibleRhythms = Array.from(rhythmKey);
     while (remaining != 0) {
-      while (possibleRhythms[possibleRhythms.length - 1][1] > remaining) {
+      while (possibleRhythms[possibleRhythms.length - 1].value > remaining) {
         possibleRhythms.pop();
         if (possibleRhythms.length == 0) throw Error('Rhythm calculation error');
       }
       const rhythm = possibleRhythms[getRandomIndex(possibleRhythms.length)];
       bar.push(rhythm);
-      remaining -= rhythm[1];
+      remaining -= rhythm.value;
     }
   }
   return bar;
